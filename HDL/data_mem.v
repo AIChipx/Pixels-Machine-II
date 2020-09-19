ee3d6659d9a878b8e485175bb15a8413a85280a4
mohamedelsayed14 <mohamedelsayed.messi@gmail.com>
2020-09-19 22:39:27 +0200

    add code

--- HDL/data_mem.v
+++ HDL/data_mem.v
@@ -1,1 +1,21 @@
-//mohamed
+//DATA_MEMORY code (data bus 128 bits) & (address bus 8 bits).
+module data_mem (clk,we,addr,data_in,data_out) ;
+ parameter addr_w=8,data_w=128;
+
+input  clk,we;
+input  [addr_w-1:0] addr; 
+input  [data_w-1:0] data_in;
+output [data_w-1:0] data_out;
+reg    [addr_w-1:0] addr_reg;
+
+reg [data_w-1:0] d_mem [2**addr_w-1:0] ;
+
+always@ (posedge clk)
+begin
+ if (we) 
+   d_mem[addr] <= data_in ;
+  
+ addr_reg <= addr ;
+end
+assign data_out = d_mem[addr_reg] ;
+endmodule 
