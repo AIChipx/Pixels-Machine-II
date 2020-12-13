e6d3a10cc6815ddd194c0d1a29427fed7d5a5b58
mohamedelsayed14 <mohamedelsayed.messi@gmail.com>
2020-12-13 13:53:12 +0200

    vertex processor v2

---
+++ HDL/vector_processor/pc.v
@@ -0,0 +1,19 @@
+//programe counter 
+// Binary counter
+
+module pc (clk,enable,reset,count);
+	parameter WIDTH=8;
+ 
+	input clk,enable,reset ;
+	output reg [WIDTH-1:0] count ;
+
+	// Reset if needed, or increment if counting is enabled
+	always @ (posedge clk or posedge reset)
+	begin
+		if (reset)
+			count <= 0;
+		else if (enable)
+			count <= count + 8'd1;
+	end
+
+endmodule
