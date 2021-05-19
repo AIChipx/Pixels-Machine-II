eeaf4546317d45dc47e7e6fefe98ed3698bc6472
mohamedelsayed14 <mohamedelsayed.messi@gmail.com>
2021-05-19 22:04:00 +0200

    Vertex_Processor_Vf1.0

--- HDL/vector_processor/data_mem.v
+++ HDL/vector_processor/data_mem.v
@@ -1,24 +1,45 @@
-//DATA_MEMORY code (data bus 128 bits) & (address bus 8 bits).
-//data memory
-module data_mem (clk,we,addr,data_in,data_out) ;
+//DATA_MEMORY code (data bus 128 bits) & (address bus 8 bits). addr= vertex read & writ address
+//data memory  
+module data_mem (clk,we,addr,data_in,data_out,
+                 vert_in,addr_inf,we_inf,vert_out) ;
 	parameter addr_w=8,data_w=128;
 
-	input  clk,we;
-	input  [addr_w-1:0] addr; 
-	input  [data_w-1:0] data_in;
-	output [data_w-1:0] data_out;
-	reg    [addr_w-1:0] addr_reg;
+	input  clk,we,we_inf;
+	input  [addr_w-1:0] addr,addr_inf; 
+	input  [data_w-1:0] data_in,vert_in;
+	output reg [data_w-1:0] data_out,vert_out;
+	
 
 reg [data_w-1:0] d_mem [2**addr_w-1:0] ;
 
+//port work on vertex
 always@ (posedge clk)
 begin
  if (we) 
-	begin	d_mem[addr] <= data_in ; end
-  
- addr_reg <= addr ;
-end
+	begin
+	d_mem[addr] <= data_in ;
+	data_out <= data_in ;
+	end
+ else
+	begin  
+   data_out <= d_mem[addr] ;
+	end
+end 
 
-assign data_out = d_mem[addr_reg] ;
+//port general data  
+always@ (posedge clk)
+begin
+ if (we_inf) 
+	begin
+	d_mem[addr_inf] <= vert_in ;
+	vert_out <= vert_in ;
+	end
+ else
+	begin  
+   vert_out <= d_mem[addr_inf] ;
+	end
+end 
+
+
 
 endmodule 
--- HDL/vector_processor/instruction_mem.v
+++ HDL/vector_processor/instruction_mem.v
@@ -1,21 +1,21 @@
 //instruction_MEMORY code (data bus 60 bits) & (address bus 8 bits).
-module instruction_mem (clk,we,addr,data_in,data_out) ;
- parameter addr_w=8,data_w=60;
+module instruction_mem (clk,we,addr_ins,addr_in,data_in,data_out) ;
+ parameter addr_w=8,data_w=15;
 
 input  clk,we;
-input  [addr_w-1:0] addr; 
+input  [addr_w-1:0] addr_ins,addr_in; 
 input  [data_w-1:0] data_in;
-output [data_w-1:0] data_out;
-reg    [addr_w-1:0] addr_reg;
+output reg[data_w-1:0] data_out;
 
 reg [data_w-1:0] instr_mem [2**addr_w-1:0] ;
 
 always@ (posedge clk)
 begin
  if (we) 
-   instr_mem[addr] <= data_in ;
+  begin
+   instr_mem[addr_in] <= data_in ;
+  end	
   
- addr_reg <= addr ;
+	data_out = instr_mem[addr_ins] ;
 end
-assign data_out = instr_mem[addr_reg] ;
 endmodule 
